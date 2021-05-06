e38c00a937709c473a8df38f023260ba6348f2c8
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-06 15:18:08 -0500

    added the CollectionItem component which captures the name, price, and image; replaced rendered div in CollectionPreview with new CollectionItem

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,15 @@
+import './collection-item.scss';
+
+const CollectionItem = ({ id, name, imageUrl, price }) => {
+    return (
+        <div key={id} className={'collection-item'}>
+            <div className={'image'} style={{ backgroundImage: `url(${imageUrl})` }} />
+            <div className={'collection-footer'}>
+                <span className={'name'}>{ name }</span>
+                <span className={'price'}>${ price }</span>
+            </div>
+        </div>
+    );
+}
+
+export default CollectionItem;
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,6 @@
+import CollectionItem from "../collection-item/collection-item.component";
 
+import './collection-preview.scss';
 
 const CollectionPreview = ({ title, items }) => {
     return (
@@ -6,9 +8,9 @@ const CollectionPreview = ({ title, items }) => {
             <h1 className={'title'}>{title.toUpperCase()}</h1>
             <div className={'preview'}>
                 {
-                    items.filter((item, index) => index < 4).map(item => {
+                    items.filter((item, index) => index < 4).map(({ id, ...otherItemProps }) => {
                        return (
-                           <div key={item.id}>{ item.name }</div>
+                           <CollectionItem key={id} {...otherItemProps} />
                        );
                     })
                 }

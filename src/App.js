3cb0eb0d02f0afc5956feba04c2e897870ce52e1
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-05 21:07:51 -0500

    added react routing to specify paths to different components/pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import {Route, Switch} from "react-router";
+
+const HatsPage = () => {
+    return (
+        <h1>HATS PAGE</h1>
+    );
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Switch>
+            <Route exact path={'/'} component={HomePage} />
+            <Route path={'/hats'} component={HatsPage} />
+        </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,8 +3,7 @@ import './menu-item.scss';
 const MenuItem = ({ title, imageUrl, size }) => {
     return (
         <div className={`${size} menu-item`}>
-            <div className={'background-image'} style={{ backgroundImage: `url(${imageUrl})` }}>
-            </div>
+            <div className={'background-image'} style={{ backgroundImage: `url(${imageUrl})` }}/>
             <div className={'content'}>
                 <h1 className={'title'}>{title.toUpperCase()}</h1>
                 <span className={'subtitle'}>Shop Now</span>

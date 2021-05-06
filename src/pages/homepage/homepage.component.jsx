c98454064567affad9017b5163f3870ab6d32b46
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-05 19:27:34 -0500

    created seperate menu and menu-item components with hover animations

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import './homepage.scss'
+
+import Directory from "../../components/menu-directory/menu-directory.component";
+
+const HomePage = () => {
+    return (
+        <div className={'homepage'}>
+            <Directory />
+        </div>
+    );
+}
+
+export default HomePage;

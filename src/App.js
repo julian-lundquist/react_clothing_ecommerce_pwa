1cf97e8b069725609a0e365f828626a0175f6fa0
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-05 21:28:45 -0500

    removed example HatsPage route

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import {Route, Switch} from "react-router";
 
-const HatsPage = () => {
-    return (
-        <h1>HATS PAGE</h1>
-    );
-}
-
 function App() {
   return (
     <div>
         <Switch>
             <Route exact path={'/'} component={HomePage} />
-            <Route path={'/hats'} component={HatsPage} />
         </Switch>
     </div>
   );

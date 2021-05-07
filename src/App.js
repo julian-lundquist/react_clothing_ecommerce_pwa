554bda51a62a4ca37a81e7074bb4802eff46d4a4
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-06 22:07:46 -0500

    added AuthenticationPage and SignInComponent which also uses a custom FormInput and CustomButton styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import {Route, Switch} from "react-router";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import AuthenticationPage from "./pages/authentication/authentication.component";
+
+import {Route, Switch} from "react-router";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Switch>
             <Route exact path={'/'} component={HomePage} />
             <Route path={'/shop'} component={ShopPage} />
+            <Route path={'/signin'} component={AuthenticationPage} />
         </Switch>
     </div>
   );
---
+++ src/pages/authentication/authentication.component.jsx
@@ -0,0 +1,10 @@
+import './authentication.scss';
+import SignInComponent from "../../components/sign-in/sign-in.component";
+
+const AuthenticationPage = () => (
+    <div className={'authentication-page'}>
+        <SignInComponent />
+    </div>
+);
+
+export default AuthenticationPage;

05c3009a648d14965986c5157db085800e77b5b6
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-11 20:42:01 -0500

    added addItem cart action method which adds an item to the cartItems redux state value

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,10 @@
 import './collection-item.scss';
 import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => {
+const CollectionItem = ({ item, addItem }) => {
+    const { id, name, imageUrl, price } = item;
     return (
         <div key={id} className={'collection-item'}>
             <div className={'image'} style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,9 +12,13 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
                 <span className={'name'}>{ name }</span>
                 <span className={'price'}>${ price }</span>
             </div>
-            <CustomButton inverted> Add to cart </CustomButton>
+            <CustomButton onClick={() => addItem(item)} inverted> Add to cart </CustomButton>
         </div>
     );
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,3 +1,4 @@
 export const CartActionTypes = {
-    TOGGLE_CART_VIEW: 'TOGGLE_CART_VIEW'
+    TOGGLE_CART_VIEW: 'TOGGLE_CART_VIEW',
+    ADD_ITEM: 'ADD_ITEM'
 }

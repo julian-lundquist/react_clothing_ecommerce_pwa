77838d5512add17ed6e3498eb379a46242d3ed89
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-12 16:50:00 -0500

    added selectCurrentUser selector for redux user and refactored all other mapStateToProps to use createStructuredSelector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+import {createStructuredSelector} from "reselect";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
     return (
@@ -14,12 +15,12 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => {
     );
 };
 
-const mapDispatchToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
 });
 
-const mapStateToProps = state => ({
-    itemCount: selectCartItemsCount(state)
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);

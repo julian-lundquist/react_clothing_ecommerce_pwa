eaaea0cba81607c82e507338d64b1672a61e943b
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-17 20:39:39 -0500

    removed old js shop data and change to mapStateToProps categoryItem naming convention

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,8 +3,8 @@ import { selectShopCategory } from "../../redux/shop/shop.selectors";
 import { connect } from "react-redux";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
-const CollectionPage = ({ categoryItem }) => {
-    const { title, items } = categoryItem;
+const CollectionPage = ({ shopCategory }) => {
+    const { title, items } = shopCategory;
     return (
         <div className={'category'}>
             <h2 className={'title'}>{title}</h2>
@@ -18,7 +18,7 @@ const CollectionPage = ({ categoryItem }) => {
 }
 
 const mapStateToProps = (state, ownProps) => ({
-    categoryItem: selectShopCategory(ownProps.match.params.categoryId)(state)
+    shopCategory: selectShopCategory(ownProps.match.params.categoryId)(state)
 });
 
 export default connect(mapStateToProps)(CollectionPage);

99718f864e922439542966aec5426b0de18bbe85
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-19 18:05:40 -0500

    created loading spinner component which loads a page after data is fetched

---
+++ src/components/loading-spinner/loading-spinner.component.jsx
@@ -0,0 +1,16 @@
+import {SpinnerContainer, SpinnerOverlay} from "./loading-spinner.styles";
+
+const LoadingSpinner = WrappedComponent => {
+    const Spinner = ({ isLoading, ...otherProps }) => {
+        return isLoading ? (
+            <SpinnerOverlay>
+                <SpinnerContainer />
+            </SpinnerOverlay>
+        ) : (
+            <WrappedComponent {...otherProps} />
+        );
+    }
+    return Spinner;
+}
+
+export default LoadingSpinner;

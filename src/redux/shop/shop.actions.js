7b99d3cbb0bcc2bd4152015a2a3aab145a82633b
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-22 23:06:29 -0500

    removed redux shop updateShopItems actions method and added errorMessage state in the reducer

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,11 +1,6 @@
 import ShopActionTypes from "./shop.types";
 import {firestore, convertShopItemsSnapshotToMap} from "../../firebase/firebase.utils";
 
-export const updateShopItems = (shopItemsMap) => ({
-    type: ShopActionTypes.UPDATE_SHOP_ITEMS,
-    payload: shopItemsMap
-});
-
 export const fetchShopItemsStart = () => ({
     type: ShopActionTypes.FETCH_SHOP_ITEMS_START
 });
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -2,7 +2,8 @@ import ShopActionTypes from "./shop.types";
 
 const INITIAL_STATE = {
     shopItems: null,
-    isFetching: false
+    isFetching: false,
+    errorMessage: undefined
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {

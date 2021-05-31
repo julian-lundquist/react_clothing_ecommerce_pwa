8ceda2ebbf8629f76888c67348579b936d416948
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-31 15:48:26 -0500

    refactored SignUpComponent to use be a functional component and use react hooks for managing the state

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -6,24 +6,20 @@ import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 import {SignUpContainer, SignUpTitle} from "./sign-up.styles";
 import {signUpStart} from "../../redux/user/user.actions";
 import { connect } from "react-redux";
+import {useState} from "react";
 
-class SignUpComponent extends React.Component {
-    constructor(props) {
-        super(props);
+const SignUpComponent = ({ signUpStart }) => {
+    const [userCredentials, setUserCredentials] = useState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
 
-        this.state = {
-            displayName: '',
-            email: '',
-            password: '',
-            confirmPassword: ''
-        }
-    }
+    const { displayName, email, password, confirmPassword } = userCredentials;
 
-    handleSubmit = async event => {
+    const handleSubmit = async event => {
         event.preventDefault();
-
-        const { signUpStart } = this.props;
-        const { displayName, email, password, confirmPassword } = this.state;
 
         if (password !== confirmPassword) {
             return;
@@ -32,31 +28,28 @@ class SignUpComponent extends React.Component {
         signUpStart({ displayName, email, password });
     }
 
-    handleChange = event => {
+    const handleChange = event => {
         const { name, value } = event.target;
 
-        this.setState({ [name]: value })
+        setUserCredentials({ ...userCredentials, [name]: value });
     }
 
-    render() {
-        const { displayName, email, password, confirmPassword } = this.state;
-        return (
-            <SignUpContainer>
-                <SignUpTitle>I do not have an account</SignUpTitle>
-                <span>Sign up with your email and password</span>
-                <form className={'sign-up-form'} onSubmit={this.handleSubmit}>
-                    <FormInput type={'text'} name={'displayName'} label={'displayName'} value={displayName} handleChange={this.handleChange} required />
-                    <FormInput type={'email'} name={'email'} label={'email'} value={email} handleChange={this.handleChange} required />
-                    <FormInput type={'password'} name={'password'} label={'password'} value={password} handleChange={this.handleChange} required />
-                    <FormInput type={'password'} name={'confirmPassword'} label={'confirmPassword'} value={confirmPassword} handleChange={this.handleChange} required />
+    return (
+        <SignUpContainer>
+            <SignUpTitle>I do not have an account</SignUpTitle>
+            <span>Sign up with your email and password</span>
+            <form className={'sign-up-form'} onSubmit={handleSubmit}>
+                <FormInput type={'text'} name={'displayName'} label={'displayName'} value={displayName} handleChange={handleChange} required />
+                <FormInput type={'email'} name={'email'} label={'email'} value={email} handleChange={handleChange} required />
+                <FormInput type={'password'} name={'password'} label={'password'} value={password} handleChange={handleChange} required />
+                <FormInput type={'password'} name={'confirmPassword'} label={'confirmPassword'} value={confirmPassword} handleChange={handleChange} required />
 
-                    <div className={'buttons'}>
-                        <CustomButton type={'submit'}>Sign Up</CustomButton>
-                    </div>
-                </form>
-            </SignUpContainer>
-        );
-    }
+                <div className={'buttons'}>
+                    <CustomButton type={'submit'}>Sign Up</CustomButton>
+                </div>
+            </form>
+        </SignUpContainer>
+    );
 }
 
 const mapDispatchToProps = dispatch => ({
